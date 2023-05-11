cd0d5341faa0894954c1d6f33412238b5b1c99aa
rehankvk <102939245+rehankvk@users.noreply.github.com>
2023-05-11 09:01:21 +0700

    lorem ipsum

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -17,11 +17,12 @@ export default {
       <div class="p-3 flex flex-col">
         <h5 class="text-2xl md:text-3xl font-bold">Hallo, Saya Fajar Raihan</h5>
         <p class="text-justify mt-2 text-base md:text-xl">
-          Saya siswa kelas 11 SMK dengan jurusan Teknik Komputer dan Jaringan
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <!-- Saya siswa kelas 11 SMK dengan jurusan Teknik Komputer dan Jaringan
           (TKJ), saya sekolah dan tinggal di Jakarta Barat. Saya memiliki minat
           dalam dunia teknologi, sebagai IT Developer terutama dibidang WEB
-          Development. Saya memiliki hobi seperti Coding dan Design.
-        </p>
+          Development. Saya memiliki hobi seperti Coding dan Design. -->
         <router-link
           to="#"
           class="mt-2 py-1 text-center text-base rounded bg-white w-1/2 text-blue-600 hover:bg-blue-600 hover:text-white border-blue-600 border"
